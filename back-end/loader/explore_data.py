86f9422f90b4647d581a59e3d1fe30591aae1772
John Murray <johnmurray@fastmail.com>
2022-10-21 10:06:16 -0400

    add mongo container

--- back-end/loader/explore_data.py
+++ back-end/loader/explore_data.py
@@ -1,27 +1,32 @@
 import fitdecode
 
+
 def convert_lat_long(val: int):
-    '''Converts a value to a latitude/longitude coordinate.'''
-    return val / ( (2**32) / 360 )
+    """Converts a value to a latitude/longitude coordinate."""
+    return val / ((2 ** 32) / 360)
+
 
 def parse_username_and_activityid(filename: str):
-    '''Parses the username and activity ID from a file name.'''
-    user_id, activity_id = filename.split('_')[0], filename.split('_')[1]
-    if '.' in activity_id:
-        activity_id = activity_id.split('.')[0]
+    """Parses the username and activity ID from a file name."""
+    user_id, activity_id = filename.split("_")[0], filename.split("_")[1]
+    if "." in activity_id:
+        activity_id = activity_id.split(".")[0]
     return user_id, activity_id
 
-data = [];
-with fitdecode.FitReader('data22.fit') as fit_file:
+
+data = []
+with fitdecode.FitReader("data22.fit") as fit_file:
     for frame in fit_file:
         if isinstance(frame, fitdecode.records.FitDataMessage):
             # print('---- BEGIN FRAME -----')
-            if frame.name == 'lap':
+            if frame.name == "activity":
                 # print('---- BEGIN LAP -----')
                 laps = []
                 for field in frame.fields:
                     # print(field.name, ':', field.value)
-                    laps.append([field.name, ':', field.value])
+                    laps.append([field.name, ":", field.value])
                 data.append(laps)
 
-print(data[5])
+print(data[0])
+
+##print(convert_lat_long(402885157), convert_lat_long(-1006486069) )
