4267c004dc5205283d94277ef4e5a72a112bac3b
John Murray <johnmurray@fastmail.com>
2022-10-21 14:19:46 -0400

    parses records, activities, and laps

--- back-end/loader/explore_data.py
+++ back-end/loader/explore_data.py
@@ -1,9 +1,13 @@
 import fitdecode
+from datetime import datetime
 
 
-def convert_lat_long(val: int):
+def convert_lat_long(val):
     """Converts a value to a latitude/longitude coordinate."""
-    return val / ((2 ** 32) / 360)
+    try:
+        return int(val) / ((2 ** 32) / 360)
+    except:
+        return val
 
 
 def parse_username_and_activityid(filename: str):
@@ -14,19 +18,33 @@ def parse_username_and_activityid(filename: str):
     return user_id, activity_id
 
 
+coords = [
+    "position_lat",
+    "position_long",
+    "start_position_lat",
+    "start_position_long",
+    "end_position_lat",
+    "end_position_long",
+]
+names = ["activity", "lap", "record"]
 data = []
 with fitdecode.FitReader("data22.fit") as fit_file:
+    objs = []
     for frame in fit_file:
-        if isinstance(frame, fitdecode.records.FitDataMessage):
-            # print('---- BEGIN FRAME -----')
-            if frame.name == "activity":
-                # print('---- BEGIN LAP -----')
-                laps = []
-                for field in frame.fields:
-                    # print(field.name, ':', field.value)
-                    laps.append([field.name, ":", field.value])
-                data.append(laps)
+        if (
+            isinstance(frame, fitdecode.records.FitDataMessage)
+            and frame.name == "activity"
+        ):
+            obj = {}  # initialize empty object
+            for field in frame.fields:
+                if isinstance(field.value, datetime):
+                    v = field.value.strftime("%Y-%m-%d %H:%M")
+                elif field.name in coords:
+                    v = convert_lat_long(field.value)
+                else:
+                    v = field.value
+                obj[field.name] = v
+            objs.append(obj)
+        data.append(objs)
 
-print(data[0])
-
-##print(convert_lat_long(402885157), convert_lat_long(-1006486069) )
+print(data[0][-1])
